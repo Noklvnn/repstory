75eb1145933848f04a598638f9434181c0e71862
Noklvnn <156442599+Noklvnn@users.noreply.github.com>
2024-01-22 21:28:30 +0300

    Add files via upload

---
+++ Laba10ovin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba10ovin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
